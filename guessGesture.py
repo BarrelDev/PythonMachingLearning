d206a55359bc46718360e416c29c4c9d92e0dd12
Barrel <47505192+BarrelDev@users.noreply.github.com>
2021-08-24 16:19:24 -0400

    Add files via upload

--- guessGesture.py
+++ guessGesture.py
@@ -7,45 +7,53 @@ import tensorflow as tf
 from tensorflow import keras
 
 def guess():
-    vdo = cv.VideoCapture(0)
-    userImg = vdo.read()
+    guessLabel = Label(frame, text = "")
+    guessLabel.pack()
+    vdo = cv.VideoCapture(0+cv.CAP_DSHOW)
+    retval,userImg = vdo.read()
     vdo.release()
-
+    # Destroy all the windows
+    cv.destroyAllWindows()
+    if retval != True:
+        print("Can't read frame")
+        guessLabel = Label(frame, text = "cant read")
+        guessLabel.pack()
+        return 5
     userImg = cv.cvtColor(userImg, cv.COLOR_BGR2GRAY)
     userImg = cv.resize(userImg, (48, 36), interpolation = cv.INTER_CUBIC)
+    cv.imshow('frame',userImg)
 
     np_img = np.asarray(userImg)
     np_img = np.delete(np_img, 2, 2)
     np_img = np.delete(np_img, 1, 2)
     np_img = np.reshape(np_img, (36, 48, 1))
 
-    model = keras.models.load_model('D:\\Python\\hand maching\\test-model-epoch5.h5')
+    model = keras.models.load_model(os.getcwd() + '\\test-model-epoch5.h5')
     probability_model = Sequential([model, tf.keras.layers.Softmax()])
     predictions = probability_model.predict(np_img)
     np.argmax(predictions[0])
     if predictions[0]==0:
-        guess = Label(frame, text = "Palm(Vertical)")
+        guessLabel = Label(frame, text = "Palm(Vertical)")
     elif predictions[0]==1:
-        guess = Label(frame, text = "Palm(Horizontal)")
+        guessLabel = Label(frame, text = "Palm(Horizontal)")
     elif predictions[0]==2:
-        guess = Label(frame, text = "1 finger")
+        guessLabel = Label(frame, text = "1 finger")
     elif predictions[0]==3:
-        guess = Label(frame, text = "Fist")
+        guessLabel = Label(frame, text = "Fist")
     elif predictions[0]==4:
-        guess = Label(frame, text = "Thumbs Up")
-    guess.pack()
+        guessLabel = Label(frame, text = "Thumbs Up")
+    guessLabel.pack()
 
 
 root = Tk()
 root.geometry("640x480")
 frame = Frame(root)
 frame.pack()
-
-label = Label(frame, text = "Click to Guess your Hand Gesture!", command = guess())
+label = Label(frame, text = "Click to Guess your Hand Gesture!")
 label.pack()
 
-testButton = Button(frame, text = "Guess")
+testButton = Button(frame, text = "Guess", command = guess)
 testButton.pack(padx = 3, pady = 3)
 
-root.title("Test")
+root.title("Guess Gesture")
 root.mainloop()
